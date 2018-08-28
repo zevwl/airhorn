bb2fbf5ec73c094424d424370ec86011dca2733b
Zev Landau <zev@landausoftware.com>
2018-08-28 14:39:16 -0400

    Updated service worker to version 0.6.11

---
+++ sw.js
@@ -0,0 +1,30 @@
+const version = '0.6.11'
+const cacheName = `airhorner-${version}`
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll([
+        '/',
+        '/index.html',
+        '/index.html?homescreen=1',
+        '/?homescreen=1',
+        '/styles/main.css',
+        '/scripts/main.min.js',
+        '/scripts/comlink.global.js',
+        '/scripts/messagechannerladapter.global.js',
+        '/scripts/pwqcompat.min.js',
+        '/sounds/airhorn.mp3'
+      ])
+        .then(() => self.skipWaiting())
+      )
+  )
+})
+
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()))
+
+self.addEventListener('fetch', event => event.respondWith(
+  caches.open(cacheName)
+    .then(cache => cache.match(event.request, {ignoreSearch: true}))
+    .then(response => response || fetch(event.request))))
+
